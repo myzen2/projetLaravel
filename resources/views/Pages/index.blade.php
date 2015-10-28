3394d188603efdbe024ef686d961442e84759e43
ouatdephoque <jeshon.assuncao@he-arc.ch>
2015-10-28 09:37:24 +0100

    create route index

---
+++ resources/views/Pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('app')
+
+@section('content')
+
+<h1>Bonjour</h1>
+<p>
+Bienvenue sur notre site. Avec nous, créez et gérez rapidement et facilement votre manifestation sportive.
+</p>
+
+@stop
