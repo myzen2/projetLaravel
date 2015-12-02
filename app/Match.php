0db0dde6a1df45b972ecde4c73b39c1b8e312ecf
leonardodistasio <leonardo.distasio@he-arc.ch>
2015-12-02 09:36:15 +0100

    Ajouter la table match et l'Eloquent (avec les fichiers c'est mieux)

---
+++ app/Match.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Match extends Model
+{
+    protected $table = 'match';
+
+    protected $fillable = [
+            'equipe1',
+            'equipe2',
+            'score1',
+            'score2',
+            'heureMatch',
+            'tournament_id'
+    ];
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2015_12_02_082359_create_match_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMatchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('match', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('equipe1', 100);
+            $table->string('equipe2', 100);
+            $table->integer('score1');
+            $table->integer('score2');
+            $table->string('heureMatch');
+            $table->integer('tournament_id')->unsigned();
+
+            $table->foreign('tournament_id')
+                  ->references('id')
+                  ->on('tournament')
+                  ->onDelete('cascade');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('match');
+    }
+}
